b61a78a58362df9adc295cc967c4f99de7086d00
hessorr <hessor@appstate.edu>
2023-10-26 12:36:24 -0400

    hooking up the recycler list to the post fragment.

--- app/src/main/java/com/example/beat_buddy/ui/post/PostFragment.kt
+++ app/src/main/java/com/example/beat_buddy/ui/post/PostFragment.kt
@@ -1,38 +1,47 @@
 package com.example.beat_buddy.ui.post
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.beat_buddy.databinding.FragmentPostBinding
+
+
+private const val TAG = "PostFragment"
 
 class PostFragment : Fragment() {
 
     private var _binding: FragmentPostBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val postsListViewModel: PostsListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total posts: ${postsListViewModel.posts.size}")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val postsListViewModel =
-            ViewModelProvider(this).get(PostsListViewModel::class.java)
-
+    ): View? {
         _binding = FragmentPostBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textPost
-        postsListViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        binding.postsRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        val posts = postsListViewModel.posts
+        val adapter = PostsListAdapter(posts)
+        binding.postsRecyclerView.adapter = adapter
+
+        return binding.root
     }
 
     override fun onDestroyView() {
